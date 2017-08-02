e8998872ce3c1f7ada16ce608ddd6df7935a144a
rodney <rodney@daftdroid.com>
2017-08-02 14:01:40 +0100

    Added copyright and license summary

--- com/daftdroid/droidlib/bind/mybind.h
+++ com/daftdroid/droidlib/bind/mybind.h
@@ -1,4 +1,22 @@
+/*
 
+(c) rodney@daftdroid.com 2017
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
 
 extern void set_thread_bind_addr_getter(int(*p)(char *));
 extern void _init(void);
